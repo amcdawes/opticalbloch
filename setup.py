d8455b5b6200f158542d5f667605069a37d345b5
Tommy Ogden <t@ogden.eu>
2014-11-24 16:44:00 +0000

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='Optical Bloch',
+      version='0.1.0',
+      description='A Python tool for solving the Optical Bloch equations for \
+                   light interacting with an ensemble of atoms.',
+      url='http://github.com/tommyogden/opticalbloch',
+      author='Tommy Ogden',
+      author_email='t@ogden.eu',
+      license='MIT',
+      packages=['opticalbloch'],
+      zip_safe=False)
